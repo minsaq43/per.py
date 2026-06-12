9d0d05419b9bebe361dc98afbd7d8554bc22f612
minsaq43 <minsaq43@gmail.com>
2026-06-12 15:26:19 +0500

    Add files via upload

---
+++ area_of_triangle.js
@@ -0,0 +1,5 @@
+\\ program to calculate the area of triangle
+var base = prompt("Enter the base of triangle: ");
+var height = prompt("Enter the height of triangle: ");
+var area = 1/2 * base * height;
+console.log(area);
